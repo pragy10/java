c9aa9e6a6436a0be267aa7adf3876130e5164283
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:51:03 +0530

    cases

---
+++ Cases.java
@@ -0,0 +1,10 @@
+public class Cases {
+    public static void main(String[] args) {
+        String fname = "john";
+        String Iname = "DoE";
+        String name = fname + Iname;
+
+        System.out.println("first name: " + fname.toUpperCase());
+        System.out.println("last name: " + Iname.toLowerCase());
+    }
+}
