d212e92f4d21ac933498c5ec9b003fdb7cf520ae
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:07:26 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+    public static void main(String[] args) {
+        int myint = 18;
+        double mydouble = myint;
+        
+        System.out.println(myint);
+        System.out.println(mydouble);
+    }
+}
