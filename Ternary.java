e7b4baf7ab61dd75ff7e94b136fda93ad9ee3e06
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:47:33 +0530

    Ternary

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 51;
+
+        y = (x == 51) ? 1 : 0;
+        System.out.println("value of yes:" + y);
+
+        y = (x == 1) ? 0 : 1;
+        System.out.println("value of y is:" + y);
+    }
+}
