4d52e8c89973ab3b3e68744061628b96e2813dad
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:15:51 +0530

    Operator

---
+++ Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = num1 + 20;
+        int num3 = num1 + num2;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
