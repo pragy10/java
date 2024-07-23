0b09bacdc059eb00187003e11b8f6e86c0a6b055
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:55:25 +0530

    Concat()

---
+++ Concat_fn.java
@@ -0,0 +1,8 @@
+public class Concat_fn {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+
+        System.out.println(firstName.concat(lastName));
+    }
+}
