05ddb678511529ded6802985e32791f1a0877eaa
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:49:27 +0530

    length

---
+++ Length.java
@@ -0,0 +1,11 @@
+public class Length {
+    public static void main(String[] args) {
+        String fname = "john";
+        String lname = "doe";
+        String name = fname + lname;
+
+        System.out.println("length of first name: " + fname.length());
+        System.out.println("length of last name: " + lname.length());
+        System.out.println("length of full name: " + name.length());
+    }
+}
