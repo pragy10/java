3ec6b7127b852cbff4b8884bb05696bab03d6ad4
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:53:13 +0530

    Index

---
+++ Index.java
@@ -0,0 +1,7 @@
+public class Index {
+    public static void main(String[] args) {
+        String txt = "Hello world! This is java programming.";
+        
+        System.out.println("The word 'java' is located at the " + txt.indexOf("java") + " index of the text");
+    }
+}
