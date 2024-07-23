6cea5c3696885e54b9859d7a3f4e878b12a80f30
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:45:46 +0530

    concatenation

---
+++ Concat.java
@@ -0,0 +1,9 @@
+public class Concat {
+    public static void main(String[] args) {
+        String fname = "john";
+        String lname = "doe";
+        String name = fname + lname;
+
+        System.out.println(name);
+    }
+}
