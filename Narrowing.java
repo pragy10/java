dd6cc70c54f67f647d7e2aa03eb54046ddeec805
pragy10 <pragya.skr10@gmail.com>
2024-07-23 21:14:08 +0530

    Narrowing

---
+++ Narrowing.java
@@ -0,0 +1,9 @@
+public class Narrowing {
+    public static void main(String[] args) {
+        double mydouble = 12.4356;
+        int myint = (int) mydouble;
+
+        System.out.println(myint);
+        System.out.println(mydouble);
+    }
+}
