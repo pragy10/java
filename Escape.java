b75b01acd59995778739df2e87dd47cd502453eb
pragy10 <pragya.skr10@gmail.com>
2024-07-23 22:01:20 +0530

    escape characters

---
+++ Escape.java
@@ -0,0 +1,6 @@
+public class Escape {
+    public static void main(String[] args) {
+        String txt = "We are the so-called \"Vikings\" from the north. Inile travel through the seas.";
+        System.out.println(txt);
+    }
+}
