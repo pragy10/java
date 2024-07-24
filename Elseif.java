bfd9ac4cb2fdca722243dc942cb8f3074f1b352d
pragy10 <pragya.skr10@gmail.com>
2024-07-24 18:52:51 +0530

    else if

---
+++ Elseif.java
@@ -0,0 +1,13 @@
+public class Elseif {
+    public static void main(String[] args) {
+        int time = 20;
+
+        if (time < 10) {
+            System.out.println("Good morning");
+        } else if (time < 20) {
+            System.out.println("Good day");
+        } else {
+            System.out.println("Good evening");
+        }
+    }
+}
