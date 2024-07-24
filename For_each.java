05c2817d1b67f6944b1f098101d7379d0b570288
pragy10 <pragya.skr10@gmail.com>
2024-07-24 19:00:13 +0530

    for each

---
+++ For_each.java
@@ -0,0 +1,9 @@
+public class For_each {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
