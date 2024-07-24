f805eb3e920520a00252eeb51b9751c67ddaf79e
pragy10 <pragya.skr10@gmail.com>
2024-07-24 18:49:51 +0530

    if else' exit

---
+++ Ifelse.java
@@ -0,0 +1,9 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        if (20 > 18) {
+            System.out.println("20 is greater than 18");
+        } else {
+            System.out.println("20 is less than 18");
+        }
+    }
+}
