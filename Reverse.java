d087dbb9d3625fa24870a9b353fad7be19481ea9
pragy10 <pragya.skr10@gmail.com>
2024-07-31 18:47:02 +0530

    Reverse using String buffer

---
+++ Reverse.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Reverse{
+	public static void main(String args[]){
+		StringBuffer sb = new StringBuffer("Hello Java");
+		sb.reverse();
+		System.out.println(sb);
+	}
+}
