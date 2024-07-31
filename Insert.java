167b83788f8bd3f8a9c31463a534b59effae3b1d
pragy10 <pragya.skr10@gmail.com>
2024-07-31 18:44:14 +0530

    insert using String buffer

---
+++ Insert.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Insert{
+	public static void main(String args[]){
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.insert(2,"Java");
+		System.out.println(sb);
+	}
+}
