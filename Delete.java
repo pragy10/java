bdbc12524e2a7e3d783acc4cb253e6144ea21b6b
pragy10 <pragya.skr10@gmail.com>
2024-07-31 18:46:07 +0530

    Delete using String buffer

---
+++ Delete.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Delete{
+	public static void main(String args[]){
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.delete(2,4);
+		System.out.println(sb);
+	}
+}
