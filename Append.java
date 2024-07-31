21727ffa484fc487259db60a8e0898649ed75a23
pragy10 <pragya.skr10@gmail.com>
2024-07-31 18:43:12 +0530

    append

---
+++ Append.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Append{
+	public static void main(String args[]){
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.append("Java");
+		System.out.println(sb);
+	}
+}
